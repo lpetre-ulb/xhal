510d5f6a3d71f08d1fadcdcf3f7b165daf3e0fb2
Jared Sturdy <sturdy@cern.ch>
2019-09-17 00:40:28 +0200

    [style] Standardize whitespace and brace style in `common/rpc` files

--- xhal/include/xhal/common/rpc/common.h
+++ xhal/include/xhal/common/rpc/common.h
@@ -24,19 +24,19 @@ namespace xhal {
   namespace common {
     namespace rpc {
 
-    /**
-     * @brief Defines the templated RPC ABI version
-     */
-    static constexpr const char* abiVersion = "v1";
+      /**
+       * @brief Defines the templated RPC ABI version
+       */
+      static constexpr const char* abiVersion = "v1";
 
-    /**
-     * @brief Class whose all remotely callable RPC method must inherit from
-     *
-     * The required inheritance is used as a compile time check so a developer
-     * cannot remotely call a local function by mistake.
-     */
-    struct Method
-    {
+      /**
+       * @brief Class whose all remotely callable RPC method must inherit from
+       *
+       * The required inheritance is used as a compile time check so a developer
+       * cannot remotely call a local function by mistake.
+       */
+      struct Method
+      {
         /**
          * @brief The operator call must be define <b>once and only once</b> per
          * RPC method.
@@ -46,16 +46,17 @@ namespace xhal {
          *
          * @warnng The call operator @b must be defined as @c const.
          */
-        template<typename R, typename... Args> R operator()(Args...) const;
-    };
+        template<typename R, typename... Args>
+        R operator()(Args...) const;
+      };
 
-    /**
-     * @brief Base of the @c MessageSerializer and @c MessageDeserializer classes
-     *
-     * @c MessageBase provides the key index tracking functionnality which
-     * is mandatory for serialization.
-     */
-    class MessageBase {
+      /**
+       * @brief Base of the @c MessageSerializer and @c MessageDeserializer classes
+       *
+       * @c MessageBase provides the key index tracking functionnality which
+       * is mandatory for serialization.
+       */
+      class MessageBase {
 
         /**
          * @brief Index to the next free/unread key
@@ -69,15 +70,15 @@ namespace xhal {
          */
         inline uint32_t dispenseKey() { return _keyIdx++; }
 
-    };
+      };
 
-    /**
-     * @brief This class serializes parameters into a @c wisc::RPCMsg
-     */
-    class MessageSerializer : public MessageBase
-    {
+      /**
+       * @brief This class serializes parameters into a @c wisc::RPCMsg
+       */
+      class MessageSerializer : public MessageBase
+      {
 
-    protected:
+      protected:
 
         wisc::RPCMsg *m_wiscMsg;
 
@@ -92,38 +93,39 @@ namespace xhal {
          *    remembering the developer that she/he can transmit defined types over the
          *    network.
          */
-        template<typename T> inline void save(const T &t) {
-            // This const_cast is safe when the API is used as intented
-            // More precisely when the object t is modified only with the operator&
-            serialize(*this, const_cast<T &>(t));
+        template<typename T>
+        inline void save(const T &t) {
+          // This const_cast is safe when the API is used as intented
+          // More precisely when the object t is modified only with the operator&
+          serialize(*this, const_cast<T &>(t));
         }
 
         /**
          * @brief Adds a @c std::uint32_t to the message
          */
         inline void save(const std::uint32_t value) {
-            m_wiscMsg->set_word(std::to_string(dispenseKey()), value);
+          m_wiscMsg->set_word(std::to_string(dispenseKey()), value);
         }
 
         /**
          * @brief Adds a @c std::vector<std::uint32_t> to the message
          */
         inline void save(const std::vector<std::uint32_t> &value) {
-            m_wiscMsg->set_word_array(std::to_string(dispenseKey()), value);
+          m_wiscMsg->set_word_array(std::to_string(dispenseKey()), value);
         }
 
         /**
          * @brief Adds a @c std::string to the message
          */
         inline void save(const std::string &value) {
-            m_wiscMsg->set_string(std::to_string(dispenseKey()), value);
+          m_wiscMsg->set_string(std::to_string(dispenseKey()), value);
         }
 
         /**
          * @brief Adds a @c std::vector<std::string> to the message
          */
         inline void save(const std::vector<std::string> &value) {
-            m_wiscMsg->set_string_array(std::to_string(dispenseKey()), value);
+          m_wiscMsg->set_string_array(std::to_string(dispenseKey()), value);
         }
 
         /**
@@ -134,45 +136,47 @@ namespace xhal {
                  typename std::enable_if<std::is_integral<T>::value && !helper::is_bool<T>::value, int>::type = 0
                 >
         inline void save(const std::array<T, N> &value) {
-            m_wiscMsg->set_binarydata(std::to_string(dispenseKey()), value.data(), N*sizeof(T));
+          m_wiscMsg->set_binarydata(std::to_string(dispenseKey()), value.data(), N*sizeof(T));
         }
 
         /**
          * @brief Adds a @c std::map<std::uint32_t, T> to the message where @c T is a serializable type
          */
-        template<typename T> inline void save(const std::map<std::uint32_t, T> &value) {
-            // The first RPC key stores the std::map keys
-            // This is required to know the std::map size at deserialization
-            const auto keysKey = dispenseKey();
+        template<typename T>
+        inline void save(const std::map<std::uint32_t, T> &value) {
+          // The first RPC key stores the std::map keys
+          // This is required to know the std::map size at deserialization
+          const auto keysKey = dispenseKey();
 
-            std::vector<std::uint32_t> keys{};
-            keys.reserve(value.size());
+          std::vector<std::uint32_t> keys{};
+          keys.reserve(value.size());
 
-            for (const auto & elem : value) {
-                keys.push_back(elem.first);
-                this->save(elem.second);
-            }
+          for (const auto & elem : value) {
+            keys.push_back(elem.first);
+            this->save(elem.second);
+          }
 
-            m_wiscMsg->set_word_array(std::to_string(keysKey), keys);
+          m_wiscMsg->set_word_array(std::to_string(keysKey), keys);
         }
 
         /**
          * @brief Adds a @c std::map<std::string, T> to the message where @c T is a serializable type
          */
-        template<typename T> inline void save(const std::map<std::string, T> &value) {
-            // The first RPC key stores the std::map keys
-            // This is required to know the std::map size at deserialization
-            const auto keysKey = dispenseKey();
+        template<typename T>
+        inline void save(const std::map<std::string, T> &value) {
+          // The first RPC key stores the std::map keys
+          // This is required to know the std::map size at deserialization
+          const auto keysKey = dispenseKey();
 
-            std::vector<std::string> keys{};
-            keys.reserve(value.size());
+          std::vector<std::string> keys{};
+          keys.reserve(value.size());
 
-            for (const auto & elem : value) {
-                keys.push_back(elem.first);
-                this->save(elem.second);
-            }
+          for (const auto & elem : value) {
+            keys.push_back(elem.first);
+            this->save(elem.second);
+          }
 
-            m_wiscMsg->set_string_array(std::to_string(keysKey), keys);
+          m_wiscMsg->set_string_array(std::to_string(keysKey), keys);
         }
 
         /**
@@ -180,8 +184,9 @@ namespace xhal {
          *
          * It should be used when setting the result from a function call.
          */
-        template<typename T> inline void save(const compat::void_holder<T> &holder) {
-            this->save(holder.get());
+        template<typename T>
+        inline void save(const compat::void_holder<T> &holder) {
+          this->save(holder.get());
         }
 
         /**
@@ -200,8 +205,8 @@ namespace xhal {
                  typename std::enable_if<I < sizeof...(Args), int>::type = 0
                 >
         inline void save(const std::tuple<Args...> &args) {
-            this->save(std::get<I>(args));
-            this->save<I+1>(args);
+          this->save(std::get<I>(args));
+          this->save<I+1>(args);
         }
 
         /**
@@ -213,7 +218,7 @@ namespace xhal {
                 >
         inline void save(const std::tuple<Args...> &) {}
 
-    public:
+      public:
 
         /**
          * @brief Constructor
@@ -227,8 +232,8 @@ namespace xhal {
          */
         template<typename T>
         inline MessageSerializer & operator<<(const T &t) {
-            this->save(t);
-            return *this;
+          this->save(t);
+          return *this;
         }
 
         /**
@@ -240,22 +245,22 @@ namespace xhal {
          */
         template<typename T>
         inline MessageSerializer & operator&(const T &t) {
-            this->save(t);
-            return *this;
+          this->save(t);
+          return *this;
         }
 
-    };
+      };
 
-    /**
-     * @brief This class deserializes parameters from a @c wisc::RPCMsg
-     *
-     * While it cannot be made @c const because deserializing requires to keep
-     * track of the state, this class guarentees that the original @c wisc::RPCMsg
-     * object will remain untouched.
-     */
-    class MessageDeserializer : public MessageBase {
+      /**
+       * @brief This class deserializes parameters from a @c wisc::RPCMsg
+       *
+       * While it cannot be made @c const because deserializing requires to keep
+       * track of the state, this class guarentees that the original @c wisc::RPCMsg
+       * object will remain untouched.
+       */
+      class MessageDeserializer : public MessageBase {
 
-    protected:
+      protected:
 
         const wisc::RPCMsg *m_wiscMsg;
 
@@ -270,36 +275,37 @@ namespace xhal {
          *    remembering the developer that she/he can transmit defined types over the
          *    network.
          */
-        template<typename T> inline void load(T &t) {
-            serialize(*this, t);
+        template<typename T>
+        inline void load(T &t) {
+          serialize(*this, t);
         }
 
         /**
          * @brief Retrieves a @c std::uint32_t from the message
          */
         inline void load(uint32_t &value) {
-            value = m_wiscMsg->get_word(std::to_string(dispenseKey()));
+          value = m_wiscMsg->get_word(std::to_string(dispenseKey()));
         }
 
         /**
          * @brief Retrieves a @c std::vector<std::uint32_t> from the message
          */
         inline void load(std::vector<std::uint32_t> &value) {
-            value = m_wiscMsg->get_word_array(std::to_string(dispenseKey()));
+          value = m_wiscMsg->get_word_array(std::to_string(dispenseKey()));
         }
 
         /**
          * @brief Retrieves a @c std::string from the message
          */
         inline void load(std::string &value) {
-            value = m_wiscMsg->get_string(std::to_string(dispenseKey()));
+          value = m_wiscMsg->get_string(std::to_string(dispenseKey()));
         }
 
         /**
          * @brief Retrieves a @c std::vector<std::string> from the message
          */
         inline void load(std::vector<std::string> &value) {
-            value = m_wiscMsg->get_string_array(std::to_string(dispenseKey()));
+          value = m_wiscMsg->get_string_array(std::to_string(dispenseKey()));
         }
 
         /**
@@ -310,33 +316,35 @@ namespace xhal {
                  typename std::enable_if<std::is_integral<T>::value && !helper::is_bool<T>::value, int>::type = 0
                 >
         inline void load(std::array<T, N> &value) {
-            m_wiscMsg->get_binarydata(std::to_string(dispenseKey()), value.data(), N*sizeof(T));
+          m_wiscMsg->get_binarydata(std::to_string(dispenseKey()), value.data(), N*sizeof(T));
         }
 
         /**
          * @brief Retrieves a @c std::map<std::uint32_t, T> from the message where @c T is a serializable type
          */
-        template<typename T> inline void load(std::map<std::uint32_t, T> &value) {
-            const auto keys = m_wiscMsg->get_word_array(std::to_string(dispenseKey()));
+        template<typename T>
+        inline void load(std::map<std::uint32_t, T> &value) {
+          const auto keys = m_wiscMsg->get_word_array(std::to_string(dispenseKey()));
 
-            for (const auto & key: keys) {
-                T val;
-                this->load(val);
-                value.emplace(key, std::move(val));
-            }
+          for (const auto & key: keys) {
+            T val;
+            this->load(val);
+            value.emplace(key, std::move(val));
+          }
         }
 
         /**
          * @brief Retrieves a @c std::map<std::string, T> from the message where @c T is a serializable type
          */
-        template<typename T> inline void load(std::map<std::string, T> &value) {
-            const auto keys = m_wiscMsg->get_string_array(std::to_string(dispenseKey()));
+        template<typename T>
+        inline void load(std::map<std::string, T> &value) {
+          const auto keys = m_wiscMsg->get_string_array(std::to_string(dispenseKey()));
 
-            for (const auto & key: keys) {
-                T val;
-                this->load(val);
-                value.emplace(key, std::move(val));
-            }
+          for (const auto & key: keys) {
+            T val;
+            this->load(val);
+            value.emplace(key, std::move(val));
+          }
         }
 
         /**
@@ -345,8 +353,9 @@ namespace xhal {
          *
          * It should be used when setting the result from a function.
          */
-        template<typename T> inline void load(compat::void_holder<T> &value) {
-            this->load(value.get());
+        template<typename T>
+        inline void load(compat::void_holder<T> &value) {
+          this->load(value.get());
         }
 
         /**
@@ -366,8 +375,8 @@ namespace xhal {
                  typename std::enable_if<I < sizeof...(Args), int>::type = 0
                 >
         inline void load(std::tuple<Args...> &args) {
-            this->load(std::get<I>(args));
-            this->load<I+1>(args);
+          this->load(std::get<I>(args));
+          this->load<I+1>(args);
         }
 
         /**
@@ -379,7 +388,7 @@ namespace xhal {
                 >
         inline void load(std::tuple<Args...> &) { }
 
-    public:
+      public:
 
         /**
          * @brief Constructor
@@ -391,10 +400,10 @@ namespace xhal {
         /**
          * @brief Allows to deserialiaze data from the message with a natural interface
          */
-        template <typename T>
+        template<typename T>
         inline MessageDeserializer & operator>>(T &t) {
-            this->load(t);
-            return *this;
+          this->load(t);
+          return *this;
         }
 
         /**
@@ -404,66 +413,66 @@ namespace xhal {
          * @c MessageDeserializer so custom types serialization can be defined in a single
          * function.
          */
-        template <typename T>
+        template<typename T>
         inline MessageDeserializer & operator&(T &t) {
-            this->load(t);
-            return *this;
+          this->load(t);
+          return *this;
         }
 
-    };
+      };
 
-    /**
-     * @brief Provides a default (de)serialiazer in case the intrusive method is used
-     */
-    template<typename Message, typename T>
-    inline void serialize(Message &msg, T &t) {
+      /**
+       * @brief Provides a default (de)serialiazer in case the intrusive method is used
+       */
+      template<typename Message, typename T>
+      inline void serialize(Message &msg, T &t) {
         t.serialize(msg);
-    }
+      }
 
-    /**
-     * @brief Serializer for @c std::array<T, N> where @c is a serializable type
-     *
-     * This a simple example of custom type serialization.
-     *
-     * The library provides two custom type serialization methods:
-     *
-     * 1. The intrusive method
-     * 2. The non-intrusive method
-     *
-     * Let's take an example :
-     *
-     * @code{.cpp}
-     * struct Point
-     * {
-     *     std::uint32_t x, y;
-     *
-     *     // The intrusive version is implemented as a new member function
-     *     // which takes a message as parameter (i.e. the serializer or deserializer)
-     *     template<class Message> inline void serialize(Message & msg) {
-     *         msg & x & y;
-     *     }
-     * };
-     *
-     * // The non-intrusive version allows to serialize objects defined in a library
-     * // Simply define the serialize function in the xhal::rpc namespace or the namespace
-     * // where the type is defined with two parameters (1) A message (i.e. the serializer
-     * // or deserializer) and (2) the custom type
-     * namespace xhal { namspace rpc {
-     *     template<typename Message> inline void serialize(Message &msg, Point &point) {
-     *         msq & point.x & point.y;
-     *     }
-     * } }
-     * @endcode
-     *
-     * @warning In order to work as intended the @c serialize functions @b MUST modify
-     * the object only with the @c operator&
-     */
-    template<typename Message, typename T, std::size_t N>
-    inline void serialize(Message &msg, std::array<T, N> &value) {
+      /**
+       * @brief Serializer for @c std::array<T, N> where @c is a serializable type
+       *
+       * This a simple example of custom type serialization.
+       *
+       * The library provides two custom type serialization methods:
+       *
+       * 1. The intrusive method
+       * 2. The non-intrusive method
+       *
+       * Let's take an example :
+       *
+       * @code{.cpp}
+       * struct Point
+       * {
+       *     std::uint32_t x, y;
+       *
+       *     // The intrusive version is implemented as a new member function
+       *     // which takes a message as parameter (i.e. the serializer or deserializer)
+       *     template<class Message> inline void serialize(Message & msg) {
+       *         msg & x & y;
+       *     }
+       * };
+       *
+       * // The non-intrusive version allows to serialize objects defined in a library
+       * // Simply define the serialize function in the xhal::rpc namespace or the namespace
+       * // where the type is defined with two parameters (1) A message (i.e. the serializer
+       * // or deserializer) and (2) the custom type
+       * namespace xhal { namspace rpc {
+       *     template<typename Message> inline void serialize(Message &msg, Point &point) {
+       *         msq & point.x & point.y;
+       *     }
+       * } }
+       * @endcode
+       *
+       * @warning In order to work as intended the @c serialize functions @b MUST modify
+       * the object only with the @c operator&
+       */
+      template<typename Message, typename T, std::size_t N>
+      inline void serialize(Message &msg, std::array<T, N> &value) {
         // The std::array size is known at compile time (and part of
         // the signature), so we don't need to serialize it
         for (auto & elem: value) {
-            msg & elem;
+          msg & elem;
         }
       }
     }
--- xhal/include/xhal/common/rpc/register.h
+++ xhal/include/xhal/common/rpc/register.h
@@ -30,11 +30,10 @@ namespace xhal {
          * In case a second exception occurs when setting the error key, @c std::terminate is called.
          */
         template<typename Exception>
-        void handleException(const Exception &e, wisc::RPCMsg *response) noexcept
-        {
-            // Log exception here?
-            response->set_string(std::string(abiVersion) + ".error", getExceptionMessage(e));
-            setExceptionType(response);
+        void handleException(const Exception &e, wisc::RPCMsg *response) noexcept {
+          // Log exception here?
+          response->set_string(std::string(abiVersion) + ".error", getExceptionMessage(e));
+          setExceptionType(response);
         }
 
         /**
@@ -42,73 +41,70 @@ namespace xhal {
          *
          * In case an exception occurs when setting the error key, @c std::terminate is called.
          */
-        void handleException(wisc::RPCMsg *response) noexcept
-        {
-            // Log exception here?
-            response->set_string(std::string(abiVersion) + ".error", "unknown exception type");
-            setExceptionType(response);
+        void handleException(wisc::RPCMsg *response) noexcept {
+          // Log exception here?
+          response->set_string(std::string(abiVersion) + ".error", "unknown exception type");
+          setExceptionType(response);
         }
 
-    } // namespace helper
+      } // namespace helper
 
-    /**
-     * @brief Locally invoke a RPC method
-     *
-     * This function is the wrapper called for every remote function call. It
-     * deserializes the arguments from the @c wisc::RPCMsg, calls the local functor
-     * and then serializes the return value to the @c wisc::RPCMsg.
-     */
-    template<typename Method,
-             typename std::enable_if<std::is_base_of<xhal::rpc::Method, Method>::value, int>::type = 0
-            >
-    void invoke(const wisc::RPCMsg *request, wisc::RPCMsg *response) noexcept
-    {
+      /**
+       * @brief Locally invoke a RPC method
+       *
+       * This function is the wrapper called for every remote function call. It
+       * deserializes the arguments from the @c wisc::RPCMsg, calls the local functor
+       * and then serializes the return value to the @c wisc::RPCMsg.
+       */
+      template<typename Method,
+               typename std::enable_if<std::is_base_of<xhal::rpc::Method, Method>::value, int>::type = 0
+              >
+      void invoke(const wisc::RPCMsg *request, wisc::RPCMsg *response) noexcept {
         try {
-            // Remove the cv-qualifiers and references since we need
-            // a copy of the object
-            helper::functor_decay_args_t<Method> args;
+          // Remove the cv-qualifiers and references since we need
+          // a copy of the object
+          helper::functor_decay_args_t<Method> args;
 
-            MessageDeserializer query(request);
-            query >> args;
+          MessageDeserializer query(request);
+          query >> args;
 
-            // Call the Method functor with the arguments received from
-            // the RPC message
-            auto result = compat::tuple_apply<helper::functor_return_t<Method>>(Method{}, args);
+          // Call the Method functor with the arguments received from
+          // the RPC message
+          auto result = compat::tuple_apply<helper::functor_return_t<Method>>(Method{}, args);
 
-            // Serialize the reply
-            MessageSerializer reply(response);
-            reply << result;
+          // Serialize the reply
+          MessageSerializer reply(response);
+          reply << result;
         } catch (const std::exception &e) {
-            helper::handleException(e, response);
+          helper::handleException(e, response);
         } catch (const wisc::RPCMsg::BadKeyException &e) {
-            helper::handleException(e, response);
+          helper::handleException(e, response);
         } catch (const wisc::RPCMsg::TypeException &e) {
-            helper::handleException(e, response);
+          helper::handleException(e, response);
         } catch (const wisc::RPCMsg::BufferTooSmallException &e) {
-            helper::handleException(e, response);
+          helper::handleException(e, response);
         } catch (const wisc::RPCMsg::CorruptMessageException &e) {
-            helper::handleException(e, response);
+          helper::handleException(e, response);
         } catch (...) {
-            helper::handleException(response);
+          helper::handleException(response);
         }
-    }
+      }
 
-    /**
-     * @brief Register a RPC method into the @c ModuleManager
-     *
-     * This helper function register a RPC method with the right parameters
-     * so it can be remotely called.
-     */
-    template<typename Method>
-    void registerMethod(ModuleManager *modmgr)
-    {
+      /**
+       * @brief Register a RPC method into the @c ModuleManager
+       *
+       * This helper function register a RPC method with the right parameters
+       * so it can be remotely called.
+       */
+      template<typename Method>
+      void registerMethod(ModuleManager *modmgr) {
         // The method name is taken from the typeid
         // This is implementation dependent but g++ and clang++
         // follow the same convention
         return modmgr->register_method(abiVersion,
                                        typeid(Method).name(),
                                        xhal::rpc::invoke<Method>);
-    }
+      }
     }
   }
 }
